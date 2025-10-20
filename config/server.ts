d3d8cf5ef8604a09f035901313b1316cbf3b0a92
BuildTools <unconfigured@null.spigotmc.org>
2025-10-20 13:01:52 +0900

    Add Tailwind setup and configuration

--- config/server.ts
+++ config/server.ts
@@ -2,11 +2,17 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
 
+  url: env('PUBLIC_URL', 'https://cms-c5fx.onrender.com'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
 
-  // ✅ HTTPS 프록시 환경 (Render) 대응
-  url: env('PUBLIC_URL', 'https://cms-c5fx.onrender.com'), // ⚠️ 본인 Render CMS 주소
-  proxy: true, // ✅ secure cookie 오류 해결용
+  // ✅ HTTPS 프록시 환경 (Render)에서도 쿠키를 허용하도록 설정
+  proxy: true,
+  settings: {
+    cookies: {
+      secure: false, // ← secure 쿠키 비활성화 (HTTP에서도 전송 가능)
+    },
+  },
 });
